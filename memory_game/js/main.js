0cbfa2db959d1faf7a5a174a6c4684934dda61ec
Isaac Rivera <IsaacR90@icloud.com>
2019-12-06 05:28:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -10,10 +31,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId) {
-    const flippedCard = cards[cardId];
-	console.log("User flipped " + flippedCard);
-	cardsInPlay.push(flippedCard);
-
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(this.cardImage);
+	console.log(this.suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
      	checkForMatch();
     }
