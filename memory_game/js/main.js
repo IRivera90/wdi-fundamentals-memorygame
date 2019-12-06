406ed70d347610669950946e2181aaeb3d314436
Isaac Rivera <IsaacR90@icloud.com>
2019-12-06 03:48:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,24 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
+
 function checkForMatch(){
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
 	}
 }
-function flipCard(cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length === 2);
-checkForMatch();
-	}
+function flipCard(cardId) {
+    const flippedCard = cards[cardId];
+	console.log("User flipped " + flippedCard);
+	cardsInPlay.push(flippedCard);
+
+	if (cardsInPlay.length === 2) {
+     	checkForMatch();
+    }
+}
+
+
 flipCard(0)
 flipCard(2)
