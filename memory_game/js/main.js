2676c444a7a83fc60b7fb493cc3d46fc7534f583
Isaac Rivera <IsaacR90@icloud.com>
2019-12-06 02:51:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,18 @@
-const cards = ["queen","queen","king","king"];
-
-const cardsInPlay = [];
-
-const cardOne = cards[0];
-cardsInPlay.push(cardOne); // [queen]
-const lastFlippedCard = cardsInPlay[cardsInPlay.length - 1]
-console.log("User flipped " + lastFlippedCard);
-
-
-const cardTwo = cards[1]; // queen
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 	}
 }
+function flipCard(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2);
+checkForMatch();
+	}
+flipCard(0)
+flipCard(2)
